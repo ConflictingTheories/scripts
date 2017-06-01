db9020a8f91d80acf1767ff9f8b9828b977e5e27
Kyle DM <confidential.inc@gmail.com>
2017-05-31 21:48:47 -0600

    cron with promises
    promisify jobs for better error checking and flow control

---
+++ examples/cron_promise.js
@@ -0,0 +1,83 @@
+var p = require('../lib/javascript/patterns.js');
+var cron = require('node-cron');
+
+
+// TEST ACTION DUMMY
+function doActionPM(ms) {
+    return new Promise((resolve, reject) => {
+        try {
+            setTimeout(() => resolve(true), ms)
+        } catch (e) {
+            reject(e);
+        }
+    });
+}
+
+// CRON WRAPPER
+function createCronJobWrapper(id, stack, cronJob) {
+    return () => {
+        let register = p.registrator(stack);
+        let start = Promise.resolve();
+        start
+            .then(() => register("", p.via(cronJob())))
+            .catch((err) => {
+                console.error("ID:", id, "\nERROR:", err)
+                stack[id].destroy();
+                process.exit();
+            });
+    }
+}
+
+// CRON JOB
+function cronJob() {
+    return new Promise((resolve, reject) => {
+        console.log("running job")
+        // Do Job Here
+        let a = 1;
+        let b = 2;
+        if (b == 2) {
+            resolve(true);
+        }
+    });
+}
+// CRON JOB
+function cronJob2() {
+    return new Promise((resolve, reject) => {
+        console.log("running job -2")
+        // Do Job Here
+        let a = 1;
+        let b = 2;
+        if (b == 2) {
+            reject("break::");
+        }
+    });
+}
+
+// CRON JOB
+function cronJob3() {
+    return new Promise((resolve, reject) => {
+        console.log("running job")
+        // Do Job Here
+        let a = 1;
+        let b = 2;
+        if (b == 2) {
+            resolve(true);
+        }
+    });
+}
+
+// MAIN LOOP
+function main() {
+    let stack = [];
+    let register = p.registrator(stack);
+    let start = Promise.resolve();
+    start
+        .then(p.from(() => register("action_1", p.via(doActionPM))))
+        .then(p.from(() => register("action_2", p.via(doActionPM))))
+        .then(p.from(() => register("job_id", p.via(cron.schedule("*/3 * * * * *", createCronJobWrapper("job_id1", stack, cronJob))))))
+        .then(p.from(() => register("job_id2", p.via(cron.schedule("*/5 * * * * *", createCronJobWrapper("job_id2", stack, cronJob2))))))
+        .then(p.from(() => register("job_id3", p.via(cron.schedule("*/2 * * * * *", createCronJobWrapper("job_id3", stack, cronJob3))))))
+        .catch((err) => console.error("ERROR:", err, "\nSTACK DUMP::", stack));
+}
+
+main()
